d0355120a2d00a1a41464055f859c85d636a921c
GeorgyPorgy <glorycorp@gmail.com>
2016-04-15 23:43:25 +0300

    rework code

--- ConsoleAppJ2534/Program.cs
+++ ConsoleAppJ2534/Program.cs
@@ -12,25 +12,52 @@ namespace ConsoleAppJ2534
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nList avalible J2534 interfaces ...");
+
+            UserInterface ConsoleApp = new UserInterface();
+
             List<PassThruDevice> AvalibleInterfaces = DetectPassThruDrv.ListDevices();
 
-            if (AvalibleInterfaces.Count == 0 )
+            int SelectedOption =  ConsoleApp.SelectJ2534Interface(AvalibleInterfaces);
+
+            if (SelectedOption != 0)
             {
-                Console.WriteLine("\nInterface was not found!");
+                ConsoleApp.ShowMessage("Failed to load interface library");
                 return;
             }
 
-            int IntfCntr = 1;
+            IPassThru Interface = DynamicPassThru.GetInstance(AvalibleInterfaces[SelectedOption - 1].FunctionLibrary);
+
+            ConsoleApp.ShowMessage("Opening Device");
+
+            Interface.Dispose();
+        }
+    }
+
+    class UserInterface
+    {
+        private int SelectedOption;
+        private int IntfCntr;
+
+        public int SelectJ2534Interface (List<PassThruDevice> AvalibleInterfaces)
+        {
+
+            Console.WriteLine("\nList of avalible interfaces:");
+
+            if (AvalibleInterfaces.Count == 0)
+            {
+                Console.WriteLine("\nInterface was not found!");
+                return 0;
+            }
+
+            IntfCntr = 1;
             foreach (PassThruDevice _interface in AvalibleInterfaces)
             {
-               
-                Console.WriteLine(string.Format("{0}.{1}      [{2}]",IntfCntr, _interface.Name, _interface.Vendor));
+
+                Console.WriteLine(string.Format("{0}.{1}      [{2}]", IntfCntr, _interface.Name, _interface.Vendor));
                 IntfCntr++;
             }
 
-
-            int SelectedOption = 0;
+            SelectedOption = 0;
             while (SelectedOption == 0)
             {
                 Console.Write("\nSelect interface : ");
@@ -41,14 +68,25 @@ namespace ConsoleAppJ2534
                 SelectedOption = 0;
             }
 
-            IPassThru Interface = DynamicPassThru.GetInstance(AvalibleInterfaces[SelectedOption - 1].FunctionLibrary);
-
-            Console.WriteLine("Opening Device");
-
-
-            Interface.Dispose();
-
-
+            return SelectedOption;
         }
+        public void ShowMessage(string Msg)
+        {
+            Console.WriteLine("\n" + Msg);
+        }
+    }
+
+    class PassThrueInterface
+    {
+        public int OpenChannel()
+        {
+            return 0;
+        }
+        
+        public int CloseChannel()
+        {
+            return 0;
+        }
+
     }
 }
